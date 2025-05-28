6ec7478bed606834efda7fc4564af7efccddd975
x14119641 <daniel.gil.romero24@gmail.com>
2025-05-28 20:09:54 +0200

    Playing with the text styles

--- lib/widgets/weekday_scroller.dart
+++ lib/widgets/weekday_scroller.dart
@@ -8,7 +8,7 @@ class WeekdayScroller extends StatelessWidget {
 
   /// Returns Monday of the current week
   DateTime getStartOfWeek(DateTime date) {
-    return date.subtract(Duration(days:date.weekday-1));
+    return date.subtract(Duration(days: date.weekday - 1));
   }
 
   /// Gwenerates 7 dates starting from Monday
@@ -21,46 +21,67 @@ class WeekdayScroller extends StatelessWidget {
   Widget build(BuildContext context) {
     final weekDates = getCurrentWeekDates();
     final dayFormatter = DateFormat('E'); // Mon, tue...
-    final dateFormatter = DateFormat('d'); 
+    final dateFormatter = DateFormat('d');
 
     return SizedBox(
-      height: 80,
+      height: 100,
       child: ListView.separated(
         itemBuilder: (context, index) {
           final date = weekDates[index];
           final isToday = DateUtils.isSameDay(date, today);
           return Container(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical:12),
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
             decoration: BoxDecoration(
-              color: isToday ? Theme.of(context).colorScheme.secondary: Theme.of(context).colorScheme.onPrimary,
+              color: isToday
+                  ? Theme.of(context).colorScheme.secondary
+                  : Theme.of(context).colorScheme.onPrimary,
               borderRadius: BorderRadius.circular(12),
             ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  dayFormatter.format(date),
-                  style: TextStyle(
-                    fontSize: 14, fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 18, right: 8),
+                      child: Text(
+                        dateFormatter.format(date),
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Theme.of(context).colorScheme.onSecondary,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 6),
+                    Text(
+                      dayFormatter.format(date),
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  dateFormatter.format(date),
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Theme.of(context).colorScheme.onSecondary,
-                  ),
-                )
+                // ticker
+                const SizedBox(height: 4),
+                date.weekday == DateTime.thursday
+                    ? Icon(
+                        Icons.check,
+                        size: 16,
+                        color: Theme.of(context).colorScheme.onSecondary,
+                      )
+                    : const SizedBox(height: 16),
               ],
             ),
           );
-        }, 
+        },
         scrollDirection: Axis.horizontal,
-        separatorBuilder: (_, __) => const SizedBox(width: 12,), 
+        separatorBuilder: (_, __) => const SizedBox(width: 12),
         itemCount: weekDates.length,
         padding: const EdgeInsets.symmetric(horizontal: 12),
-        ),
+      ),
     );
   }
 }
